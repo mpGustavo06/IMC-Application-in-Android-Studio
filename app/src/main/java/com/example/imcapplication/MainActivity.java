73dca2a88e36e8bc30dde8dd187fb1e7617bf713
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2024-10-30 21:15:50 -0300

    Create Layout

--- app/src/main/java/com/example/imcapplication/MainActivity.java
+++ app/src/main/java/com/example/imcapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.imcapplication;
 
 import android.os.Bundle;
+import android.view.Menu;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -8,7 +10,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
